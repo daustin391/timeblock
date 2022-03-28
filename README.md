5de79d4b7c365c4c0b47514438518dc6b23fc541
Dave Austin <d.austin_391@yahoo.com>
2022-03-28 17:00:27 -0400

    take input

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # timeblock
+This is a text-based timeblocking script written in Python 3.9.6.
---
+++ timeblock.py
@@ -0,0 +1,15 @@
+""" Timeblock
+    A script for breaking up a day into scheduled blocks of time
+
+"""
+def main():
+    """Prompt for input and print schedule"""
+    while True:
+        item = input()
+        if item.casefold() == 'x':
+            break
+
+
+
+if __name__ == "__main__":
+    main()
